6c58dfb26fdc65f86ef46eea943921141bd2c58b
Freddy <drennanfreddy@gmail.com>
2022-04-30 22:11:52 -0600

    save

--- app.R
+++ app.R
@@ -7,7 +7,7 @@ devtools::load_all()
 
 router <- make_router(
   route("/", ui_home()),
-  route("shinyace", ui_shiny_ace()),
+  # route("shinyace", ui_shiny_ace()),
   route("about", h1("About", class = "display-1")),
   route("theme", bs_text_ui()),
   route("settings", ui_settings("settings", "testuser"))
@@ -32,7 +32,7 @@ ui <- function(incoming) {
 server <- function(input, output, session) {
   router$server(input, output, session)
   server_home()
-  server_shiny_ace()
+  # server_shiny_ace()
   server_navbar()
   server_footer()
 }
