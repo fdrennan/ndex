1c5ed26aa1515265b4707a2b6fc78b5e583a2625
Freddy Drennan <drennanfreddy@gmail.com>
2022-05-09 13:36:36 -0600

    save

--- backend/plumber.R
+++ backend/plumber.R
@@ -8,12 +8,14 @@
 #
 
 library(ndexback)
+
+
 #* @apiTitle Plumber Example API
 #* @apiDescription Plumber example description.
 
 #* Echo back the input
 #* @param code Code to execute
-#* @get /code
+#* @get /code/rmarkdown
 #* @serializer html
 function(code = "print(mtcars)") {
   prefix <- '---
@@ -34,6 +36,32 @@ html_document:
   render(tf, output_file = tfout, output_format = "html_document")
   tmp <- paste0(tfout, ".html")
   readBin(tmp, "raw", n = file.info(tmp)$size)
+}
+
+#* Echo back the input
+#* @param code Code to execute
+#* @get /code/markdown
+#* @serializer html
+function(code = "print(mtcars)") {
+  eval_code <- paste0("\n```{r echo = TRUE, comment = NA}\n", code, "\n```\n")
+  ace_envir <- environment()
+
+  # browser()
+  tfout <- tempfile()
+  tmp <- paste0(tfout, ".html")
+  file <- knitr::knit2html(
+    text = eval_code,
+    # output = tmp,
+    fragment.only = TRUE,
+    quiet = FALSE,
+    envir = ace_envir
+  )
+  tf <- tempfile()
+  fileConn <- file(tf)
+  writeLines(file, fileConn)
+  close(fileConn)
+  readBin(tf, "raw", n = file.info(tf)$size)
+
 }
 
 #* Plot a histogram
