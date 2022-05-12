2d4f1fbdd1777de280167ed164c73ae2111b7ef3
Freddy <drennanfreddy@gmail.com>
2022-05-12 17:18:03 -0600

    save

--- backend/plumber.R
+++ backend/plumber.R
@@ -62,6 +62,32 @@ function(req, res) {
 }
 
 
+#* Echo back the input
+#* @param code Code to execute
+#* @get /code/markdown
+#* @serializer html
+function(code = "print(mtcars)") {
+  eval_code <- paste0("\n```{r echo = TRUE, comment = NA}\n", code, "\n```\n")
+  ace_envir <- environment()
+
+  # browser()
+  tfout <- tempfile()
+  tmp <- paste0(tfout, ".html")
+  file <- knitr::knit2html(
+    text = eval_code,
+    # output = tmp,
+    fragment.only = TRUE,
+    quiet = FALSE,
+    envir = ace_envir
+  )
+  tf <- tempfile()
+  fileConn <- file(tf)
+  writeLines(file, fileConn)
+  close(fileConn)
+  readBin(tf, "raw", n = file.info(tf)$size)
+}
+
+
 # Programmatically alter your API
 #* @plumber
 function(pr) {
