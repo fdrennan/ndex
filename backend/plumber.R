29760eb4ea580e1d1f2008c48eb1da0e77a68247
Freddy <drennanfreddy@gmail.com>
2022-05-12 16:58:21 -0600

    save

--- backend/plumber.R
+++ backend/plumber.R
@@ -9,82 +9,58 @@
 
 library(ndexback)
 
+#* @filter cors
+cors <- function(req, res) {
+
+  res$setHeader("Access-Control-Allow-Origin", "*")
+
+  if (req$REQUEST_METHOD == "OPTIONS") {
+    res$setHeader("Access-Control-Allow-Methods","*")
+    res$setHeader("Access-Control-Allow-Headers", req$HTTP_ACCESS_CONTROL_REQUEST_HEADERS)
+    res$status <- 200
+    return(list())
+  } else {
+    plumber::forward()
+  }
+
+}
 
 #* @apiTitle Plumber Example API
 #* @apiDescription Plumber example description.
 
-#* Echo back the input
-#* @param code Code to execute
-#* @get /code/rmarkdown
-#* @serializer html
-function(code = "print(mtcars)") {
-  prefix <- '---
-title: ""
-output:
-html_document:
-  toc: yes
----
-```{r}'
-  postfix <- "```"
-  #
-  code <- paste0(c(prefix, code, postfix), collapse = "\n")
-  tf <- tempfile()
-  tfout <- tempfile()
-  fileConn <- file(tf)
-  writeLines(code, fileConn)
-  close(fileConn)
-  render(tf, output_file = tfout, output_format = "html_document")
-  tmp <- paste0(tfout, ".html")
-  readBin(tmp, "raw", n = file.info(tmp)$size)
-}
 
-#* Echo back the input
-#* @param code Code to execute
-#* @get /code/markdown
-#* @serializer html
-function(code = "print(mtcars)") {
-  eval_code <- paste0("\n```{r echo = TRUE, comment = NA}\n", code, "\n```\n")
-  ace_envir <- environment()
-
-  #
-  tfout <- tempfile()
-  tmp <- paste0(tfout, ".html")
-  file <- knitr::knit2html(
-    text = eval_code,
-    # output = tmp,
-    fragment.only = TRUE,
-    quiet = FALSE,
-    envir = ace_envir
-  )
-  tf <- tempfile()
-  fileConn <- file(tf)
-  writeLines(file, fileConn)
-  close(fileConn)
-  readBin(tf, "raw", n = file.info(tf)$size)
-}
-
-#* Plot a histogram
+#* Cookie Exchange
 #* @serializer json
-#* @get /email
-function(from, passwd, to = "<fr904103@bmrn.com>", subject = "test", body = "This was sent from R", smtpServer = "mail.bmrn.com") {
-  # library(mailR)
-  # library(rJava)
-  # Install Java
-  # https://www.oracle.com/java/technologies/downloads/#jdk17-windows
-  # Allow "less secure apps"
-  # https://myaccount.google.com/security
+#* @get /login
+function(req, res, user='user', password='password') {
 
-  # Sys.setenv(JAVA_HOME="C:/Program Files/Java/jdk-17.0.2/")
+  session_id <- req$cookies$session_id
+  if (!is.null(session_id)) {
+    message('Using existing session id')
+    glue('Session {session_id}')
+  } else {
+    # Create New Session
+    message('Create a new session_id')
+    session_id <-  UUIDgenerate()
+  }
 
-  # from <- sprintf("<fr904103@bmrn.com>","Freddy Drennan") # the sender’s name is an optional value
-  # to <- c("<fr904103@bmrn.com>", "<cheng.su@bmrn.com>")
-  # subject <- "Email test within Docker on HPC"
-  # body <- "Test Was Successful"
+  res$setCookie('session_id', session_id)
 
-  sendmail(front, to, subject, body, control = list(smtpServer = smtpServer))
-
-  toJSON(TRUE)
+  list(session_id = session_id)
 }
+
+#* Cookie Exchange
+#* @serializer json
+#* @get /logout
+function(req, res) {
+
+  session_id <- req$cookies$session_id
+  res$removeCookie('session_id')
+  list(
+    session_id = session_id
+  )
+}
+
 
 # Programmatically alter your API
 #* @plumber
