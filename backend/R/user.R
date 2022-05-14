082f99ec46740234fd53e8c5b6240b13492832e1
Freddy <drennanfreddy@gmail.com>
2022-05-13 19:20:45 -0600

    save

--- backend/R/user.R
+++ backend/R/user.R
@@ -1,22 +1,96 @@
-#' code_markdown
+#' user_create
 #' @export
-code_markdown <- function(code = "print(mtcars)") {
-  eval_code <- paste0("\n```{r echo = TRUE, comment = NA}\n", code, "\n```\n")
-  ace_envir <- environment()
+user_create <- function(req, res, email, hash, phone_number = "", time_zone = "") {
+  res$removeCookie("session_id")
 
-  # browser()
-  tfout <- tempfile()
-  tmp <- paste0(tfout, ".html")
-  file <- knitr::knit2html(
-    text = eval_code,
-    # output = tmp,
-    fragment.only = TRUE,
-    quiet = FALSE,
-    envir = ace_envir
+  # Create data to add user
+  user_info <- data.frame(
+    email = email,
+    hash = hash,
+    created = Sys.time(),
+    role = "user",
+    id = UUIDgenerate(),
+    phone_number = phone_number,
+    time_zone = time_zone
   )
-  tf <- tempfile()
-  fileConn <- file(tf)
-  writeLines(file, fileConn)
-  close(fileConn)
-  readBin(tf, "raw", n = file.info(tf)$size)
+
+  con <- connect_table("lite", "sqlite.db")
+  on.exit(dbDisconnect(con))
+  # browser()
+  if (isFALSE(dbExistsTable(con, "users"))) {
+    dbCreateTable(con, "users", user_info)
+  } else {
+    message("Checking that user doesn't exist.")
+    users <- tbl(con, "users") %>%
+      filter(email %in% local(email)) %>%
+      collect()
+    return(
+      list(user_exists = TRUE)
+    )
+  }
+
+  dbAppendTable(con, "users", user_info)
+
+  session_id <- UUIDgenerate()
+  session <- data.frame(
+    email = email,
+    session_id = session_id,
+    logged_id = TRUE,
+    created = Sys.time()
+  )
+
+  if (isFALSE(dbExistsTable(con, "sessions"))) {
+    dbCreateTable(con, "sessions", session)
+  }
+
+  dbAppendTable(con, "sessions", session)
+
+  res$setCookie("session_id", session_id)
+  res$status <- 303 # redirect
+  res$setHeader("Location", glue(
+    "https://ndexr.com/#!/home?session_id={session_id}"
+  ))
+  return(
+    list(user_exists = TRUE)
+  )
+}
+
+#' user_login
+#' @export
+user_login <- function(req, res, email, password) {
+  res$removeCookie("session_id")
+  con <- connect_table("lite", "sqlite.db")
+  on.exit(dbDisconnect(con))
+
+  users <- tbl(con, "users") %>%
+    filter(email %in% local(email)) %>%
+    collect()
+  user_exists <- nrow(users) == 1
+  hash_match <- checkpw(password, users$hash)
+  res$status <- 303 # redirect
+  session_id <- UUIDgenerate()
+  if (all(user_exists, hash_match)) {
+    res$setCookie("session_id", session_id)
+    res$setHeader("Location", glue(
+      "https://ndexr.com/#!/home?session_id={session_id}"
+    ))
+  } else {
+    res$removeCookie("session_id")
+    res$setHeader("Location", glue(
+      "https://ndexr.com/#!/login"
+    ))
+  }
+  list(session_id = session_id)
+}
+
+#' user_logout
+#' @export
+user_logout <- function(req, res) {
+  session_id <- req$cookies$session_id
+  res$removeCookie("session_id")
+  res$status <- 303 # redirect
+  res$setHeader("Location", "https://ndexr.com/#!/login")
+  list(
+    session_id = session_id
+  )
 }
